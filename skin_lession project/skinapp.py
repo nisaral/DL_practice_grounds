2deb1b8f6393746b308af9ca3c49b82614784b9b
keyush <nisarkeyush3@gmail.com>
2024-11-03 16:24:55 +0530

    flask

---
+++ skin_lession project/skinapp.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import os
+
+app = Flask(__name__)
+
+
+model = load_model('')  
+
+# Define the image size based on your model's input
+IMG_SIZE = (224, 224)
+
+@app.route('/')
+def home():
+    return "<h1>skin lesion Classification API</h1>"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+    
+    # Load and preprocess the image
+    img_path = os.path.join('uploads', file.filename)
+    file.save(img_path)
+
+    img = image.load_img(img_path, target_size=IMG_SIZE)
+    img_array = image.img_to_array(img) / 255.0  # Normalize the image
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+
+    # Make predictions
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions, axis=1)
+
+    # Return the predicted class as a response
+    return jsonify({'predicted_class': int(predicted_class[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
